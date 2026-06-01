e1a03b33c24a2b802651e7fa724b21c84062e402
ThiagoStuginski <thiagostuginski@discente.ufg.br>
2026-06-01 01:16:24 -0300

    Create const_pipeline.py

---
+++ pipeline_producer-consumer/const_pipeline.py
@@ -0,0 +1,6 @@
+# Modifique com os IPs reais de cada máquina da sua rede distribuída
+IP_ESTAGIO_1 = "192.168.1.10"  # Máquina que vai rodar o gerador
+IP_ESTAGIO_2 = "192.168.1.11"  # Máquina que vai rodar o intermediário
+
+PORT_1 = "5555" # Porta de comunicação entre Estágio 1 e Estágio 2
+PORT_2 = "5556" # Porta de comunicação entre Estágio 2 e Estágio 3
